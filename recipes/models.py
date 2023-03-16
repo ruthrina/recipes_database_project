b73853c12c5a0d183b6c4e3c1421319f66afb5c0
jod35 <jodestrevin@gmail.com>
2023-03-16 12:06:59 +0300

    remove bootstrap

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class RecipeChoices(models.TextChoices):
+    DESSERTS = "desserts" , "D"
+    ENTREES = "entrees" , "E"
+    APPETIZERS = "appetizers" , "A"
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=50)
+    detail =models.TextField()
+    date_created =models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    category = models.CharField(max_length=10,choices=RecipeChoices.choices,default=RecipeChoices.APPETIZERS)
+    image = models.ImageField(upload_to="images/",default="default.jpg")
+
+    def __str__(self):
+        return self.name
+    
+
+    class Meta:
+        ordering = ['-date_created']
+
+
